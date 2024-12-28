12ab5a870c616902e969893ee0baae29e085e357
Victor5204 <Victor52.04@mail.ru>
2024-12-28 13:59:28 +0300

    Добавьте файлы проекта.

---
+++ Rieltor/LoginWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Rieltor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Rieltor
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        private RieltorEntities _db;
+
+        public LoginWindow()
+        {
+            InitializeComponent();
+            _db = new RieltorEntities();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string login = LoginTextBox.Text;
+            string password = PasswordBox.Password;
+
+            var user = _db.Пользователи.FirstOrDefault(u => u.Логин == login && u.Пароль == password);
+
+            if (user != null)
+            {
+                MessageBox.Show($"Добро пожаловать, {user.Логин}!");
+                OpenMainWindow(user.Роль);
+            }
+            else
+            {
+                MessageBox.Show("Неверный логин или пароль!");
+            }
+        }
+
+        private void OpenMainWindow(string role)
+        {
+            var mainWindow = new MainWindow(role);
+            mainWindow.Show();
+            this.Close();
+        }
+    }
+}
---
+++ Rieltor/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using Rieltor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Rieltor
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private RieltorEntities _db;
+        private string _role;
+
+        // Конструктор по умолчанию
+        public MainWindow()
+        {
+            InitializeComponent();
+            _db = new RieltorEntities();
+            _role = "Гость"; // Значение по умолчанию
+            LoadData();
+            SetVisibility();
+        }
+
+        // Конструктор с параметром
+        public MainWindow(string role) : this()
+        {
+            _role = role;
+        }
+
+        private void LoadData()
+        {
+            ClientsGrid.ItemsSource = _db.Клиенты.ToList();
+            ContractsGrid.ItemsSource = _db.Договора.ToList();
+
+            if (_role == "Администратор")
+            {
+                UsersGrid.ItemsSource = _db.Пользователи.ToList();
+            }
+        }
+
+        private void SetVisibility()
+        {
+            if (_role != "Администратор")
+            {
+                var tabItem = (TabItem)FindName("UsersTab");
+                if (tabItem != null)
+                {
+                    tabItem.Visibility = Visibility.Collapsed;
+                }
+            }
+        }
+    }
+}
